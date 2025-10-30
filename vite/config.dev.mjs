1b1084ed2cb456151ddcd3958dcc10633d34c9e0
GalMarkelj <galmarkelj09@gmail.com>
2025-10-30 12:35:52 +0100

    feat: dockerize the application

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import process from 'node:process'
 
 export default defineConfig({
-  base: "./",
+  base: './',
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          phaser: ["phaser"],
+          phaser: ['phaser'],
         },
       },
     },
   },
   server: {
-    port: 8080,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT),
   },
-});
+})
